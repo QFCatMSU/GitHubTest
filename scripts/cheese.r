a350762c2062da0d899a3a7b477c8c3a7e0096e9
Charlie <belinsky@msu.edue>
2021-06-02 12:08:15 -0400

    Added cheese.r and llama.r

---
+++ scripts/cheese.r
@@ -0,0 +1,20 @@
+{
+  cheeseAns = readline("What is the best cheese in the world?");
+  
+  if(cheeseAns == "swiss") 
+  {
+    cat("Swiss is so bland");
+  }
+  else if(cheeseAns == "cheddar") 
+  {
+    cat("Cheddar is good, but there is better.");
+  }
+  else if(cheeseAns == "Meunster") 
+  {
+    cat("Ah, yes, you are a true cheese aficionado!"); 
+  }
+  else 
+  {
+    cat("Sorry, I did not understand your response!");
+  }
+}
---
+++ scripts/llama.r
@@ -0,0 +1,1 @@
+cat("I love lamas!")
